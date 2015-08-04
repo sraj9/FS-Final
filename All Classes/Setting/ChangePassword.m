8183940ee55f15684cce4f45cb49c00592ec9749
indianic <indianic@ind697.local>
2015-08-04 15:19:44 -0700

    no message

---
+++ All Classes/Setting/ChangePassword.m
@@ -0,0 +1,37 @@
+//
+//  ChangePassword.m
+//  FriendsSpotFinal
+//
+//  Created by indianic on 04/08/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import "ChangePassword.h"
+
+@interface ChangePassword ()
+
+@end
+
+@implementation ChangePassword
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
