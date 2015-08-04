3596a8448a6d6bd46c72c41764e72f95871838f6
indianic <indianic@ind697.local>
2015-08-04 15:23:36 -0700

    no message

---
+++ All Classes/Setting/EditProfile.h
@@ -0,0 +1,13 @@
+//
+//  EditProfile.h
+//  FriendsSpotFinal
+//
+//  Created by indianic on 04/08/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfile : UIViewController
+
+@end
