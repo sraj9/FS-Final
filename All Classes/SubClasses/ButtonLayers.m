b12c5120ef8d02d3a232380581b7ce837bada39c
indianic <indianic@ind697.local>
2015-08-04 15:34:44 -0700

    no message

---
+++ All Classes/SubClasses/ButtonLayers.m
@@ -0,0 +1,21 @@
+//
+//  ButtonLayers.m
+//  FriendsSpotFinal
+//
+//  Created by indianic on 04/08/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import "ButtonLayers.h"
+
+@implementation ButtonLayers
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
