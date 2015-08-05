bb59bf7f77418a45033c3599d3dfba94fa58ef76
indianic <indianic@ind697.local>
2015-08-04 18:15:09 -0700

    Today Last

--- All Classes/Login/Login.m
+++ All Classes/Login/Login.m
@@ -14,6 +14,8 @@
 
 @implementation Login
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
   
@@ -61,8 +63,11 @@
     }
     else
     {
-        NSURL *aurl =[NSURL URLWithString:@"http://friendsgrs.net46.net/?uName=govind300&password=1&action=login"];
-        NSMutableDictionary *adic=[[NSMutableDictionary alloc]initWithObjects:[NSArray arrayWithObjects:_txtUserName.text,_txtPassword.text, nil] forKeys:[NSArray arrayWithObjects:@"uName",@"password", nil]];
+        NSURL *aurl =[NSURL URLWithString:@"http://friendsgrs.net46.net"];
+        
+//        /?uName=govind300&password=1&action=login
+        NSMutableDictionary *adic=[[NSMutableDictionary alloc]initWithObjects:[NSArray arrayWithObjects:_txtUserName.text,_txtPassword.text,@"login", nil] forKeys:[NSArray arrayWithObjects:@"uName",@"password",@"action",nil]];
+        
         NSData *adata=[NSJSONSerialization dataWithJSONObject:adic options:NSJSONWritingPrettyPrinted error:nil];
         
         NSMutableURLRequest *aMutReq=[[NSMutableURLRequest alloc]initWithURL:aurl cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10.0];
@@ -70,10 +75,25 @@
         [aMutReq setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
         [aMutReq setHTTPBody:adata];
         [aMutReq setValue:[NSString stringWithFormat:@"%lu",(unsigned long)[adata length]] forHTTPHeaderField:@"Content-Length"];
+        NSLog(@"Data %@",adata);
         
+        NSString *astrdata =[[NSString alloc] initWithData:adata encoding:NSUTF8StringEncoding];
+        NSLog(@"Data String%@",astrdata);
         
-    }
+    
 
+        //Asynchronous WebCall With Blocks
+    
+    [NSURLConnection sendAsynchronousRequest:aMutReq queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+         {
+             if (data.length > 0 && connectionError == nil)
+             {
+                NSLog(@"%@",[NSString stringWithFormat:@"%@",[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil]]);
+                 
+             }
+         }];
+    }
+    
 }
 
 /*
