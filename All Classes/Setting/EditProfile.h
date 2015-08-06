c98a95e4ae5bf9eab18eedd198e041537cd4b781
Sumit <gajjarsumit143@gmail.com>
2015-08-05 18:02:17 -0700

    no message

--- All Classes/Setting/EditProfile.h
+++ All Classes/Setting/EditProfile.h
@@ -14,5 +14,18 @@
 @property (weak, nonatomic) IBOutlet UIButton *btnEditclick;
 @property (weak, nonatomic) IBOutlet UIButton *btnDoneclick;
 @property (weak, nonatomic) IBOutlet UILabel *lblFname;
+@property (weak, nonatomic) IBOutlet UILabel *lblCity;
+@property (weak, nonatomic) IBOutlet UILabel *lblState;
+@property (weak, nonatomic) IBOutlet UILabel *lblCountry;
+@property (weak, nonatomic) IBOutlet UILabel *lblContact;
+@property (weak, nonatomic) IBOutlet UITextField *txtCity;
+@property (weak, nonatomic) IBOutlet UITextField *txtState;
+@property (weak, nonatomic) IBOutlet UITextField *txtCountry;
+@property (weak, nonatomic) IBOutlet UITextField *txtContact;
+
+
+
+
+
 
 @end
--- All Classes/Setting/EditProfile.m
+++ All Classes/Setting/EditProfile.m
@@ -17,15 +17,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.txtname.hidden=YES;
-    self.btnDoneclick.hidden=YES;
     
-    // Do any additional setup after loading the view.
+    
+    self.btnDoneclick.hidden=YES;
+    self.txtname.hidden=YES;
+    self.txtCity.hidden=YES;
+    self.txtContact.hidden=YES;
+    self.txtCountry.hidden=YES;
+    self.txtState.hidden=YES;
+
+    
+        // Do any additional setup after loading the view.
+}
+-(BOOL)textFieldShouldReturn:(UITextField *) textField{
+    
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+    [self.view endEditing:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  // Dispose of any resources that can be recreated
 }
 
 /*
@@ -37,17 +54,34 @@
     // Pass the selected object to the new view controller.
 }
 */
-
 - (IBAction)btneditclick:(id)sender {
     self.lblFname.hidden=YES;
+    self.lblCity.hidden=YES;
+    self.lblState.hidden=YES;
+    self.lblCountry.hidden=YES;
+    self.lblContact.hidden=YES;
     self.txtname.hidden=NO;
+    self.txtCity.hidden=NO;
+    self.txtState.hidden=NO;
+    self.txtCountry.hidden=NO;
+    self.txtContact.hidden=NO;
+
     self.btnDoneclick.hidden=NO;
     self.btnEditclick.hidden=YES;
     
 }
 - (IBAction)btnDoneclick:(id)sender {
     self.txtname.hidden=YES;
+    self.txtCity.hidden=YES;
+    self.txtState.hidden=YES;
+    self.txtCountry.hidden=YES;
+    self.txtContact.hidden=YES;
     self.lblFname.hidden=NO;
+    self.lblCity.hidden=NO;
+    self.lblState.hidden=NO;
+    self.lblCountry.hidden=NO;
+    self.lblContact.hidden=NO;
+    
     self.btnDoneclick.hidden=YES;
     self.btnEditclick.hidden=NO;
     
