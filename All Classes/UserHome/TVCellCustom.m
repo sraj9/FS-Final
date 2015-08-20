b1245cce0987a8d1d261c0a274443973fd48b31f
Govind <indianic@ind698.local>
2015-08-19 17:29:01 -0700

    no message

---
+++ All Classes/UserHome/TVCellCustom.m
@@ -0,0 +1,23 @@
+//
+//  TVCellCustom.m
+//  FriendsSpotFinal
+//
+//  Created by indianic on 19/08/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import "TVCellCustom.h"
+
+@implementation TVCellCustom
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
