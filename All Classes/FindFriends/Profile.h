ddaaf6e03cb565be76e9e2ac2f7f1ca9d4b9a296
indianic <indianic@ind694.local>
2015-08-20 17:31:39 -0700

    no message

---
+++ All Classes/FindFriends/Profile.h
@@ -0,0 +1,26 @@
+//
+//  Profile.h
+//  FriendsSpotFinal
+//
+//  Created by indianic on 20/08/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIImageView+WebCache.h"
+
+@interface Profile : UIViewController
+{
+    
+}
+@property (strong,nonatomic) NSMutableDictionary *profileDetails;
+@property (weak, nonatomic) IBOutlet UIImageView *imgUserProfle;
+@property (weak, nonatomic) IBOutlet UILabel *lblName;
+@property (weak, nonatomic) IBOutlet UILabel *lblSirname;
+@property (weak, nonatomic) IBOutlet UILabel *lblGender;
+@property (weak, nonatomic) IBOutlet UILabel *lblCity;
+@property (weak, nonatomic) IBOutlet UILabel *lblState;
+@property (weak, nonatomic) IBOutlet UILabel *lblCountry;
+@property (weak, nonatomic) IBOutlet UILabel *lblContact;
+
+@end
